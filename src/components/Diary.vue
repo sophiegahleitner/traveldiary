368d30bdb98a16af74cb3ccc83214d6d105c1f91
Sophie Gahleitner <sophie.gahleitner@gmail.com>
2019-03-14 19:15:25 +0100

    my changes

--- src/components/Diary.vue
+++ src/components/Diary.vue
@@ -2,7 +2,7 @@
     <li class="diary">
         <router-link :to="{ name: 'diary', params:{id: id} }">
             <div class="diary-image">
-                <img alt="Vue logo" src="../assets/example.jpg">
+                <img alt="Vue logo" src="../assets/img/example.jpg">
             </div>
             <div class="diary-facts">
                 <h2>{{country}}</h2>
@@ -13,16 +13,7 @@
 </template>
 
 <style>
-    .diary {
-        width: calc(33% - 60px);
-        margin: 30px;
-        box-shadow: 1px 5px 15px #ddd;
-        display: inline-block;
-    }
 
-    .diary-facts {
-        padding: 10px;
-    }
 </style>
 
 <script>
---
+++ vue.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  presets: [
+    '@vue/app'
+  ],
+/*
+    module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                use: [
+                    'vue-style-loader',
+                    'css-loader',
+                    'sass-loader'
+                ]
+            }
+        ]
+    },
+    */
+};
