cdf255d697a90c2dfd6e3c48249d529c63f96dc4
MichaelSieghartsleitenr <s1510456035@students.fh-hagenberg.at>
2019-03-25 18:45:59 +0100

    fix

--- src/main.js
+++ src/main.js
@@ -2,9 +2,8 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import './registerServiceWorker'
-import './assets/scss/main.scss';
-import store from './store/store.js';
-
+import store from './store/store.js'
+// import serviceworker from 'registerServiceWorker'
 Vue.config.productionTip = false;
 
 new Vue({
